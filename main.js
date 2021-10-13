68010c6ac854cb298368cfb08f03ee286610fec7
gfrancoa <gfrancoa@outlook.com>
2021-10-12 20:54:38 -0500

    creando json

--- main.js
+++ main.js
@@ -1,1 +1,53 @@
-alert("Hola");
+// alert("Hola");
+
+const ventas_facturacion =[
+    {
+        numeroDeFactura:0005,
+        cliente:{
+            id:1,
+            nombre:''
+        }
+    }
+]
+
+const pedidos
+
+const gastos
+
+const compras
+
+const clientes
+
+const mesas
+
+const categorias
+
+const reporte_de_inventario
+
+const usuarios
+
+const rol
+
+const permisos
+
+const 
+
+const productos = [
+    {
+        id:1,
+        nombre:'Ensalada',
+        precio:12000,
+    },
+
+    {
+        id:2,
+        nombre:'Pollo asado',
+        precio:10000, 
+    },
+
+    {
+        id:3,
+        nombre:'Flan',
+        precio:5000,
+    }
+]
