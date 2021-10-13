0f9815b0854cc66442201f1e7ae3b65f9b3220d4
jayg1503 <91357346+jayg1503@users.noreply.github.com>
2021-10-12 21:48:47 -0500

    Se actualiza JS de Restaurante

--- main.js
+++ main.js
@@ -5,49 +5,157 @@ const ventas_facturacion =[
         numeroDeFactura:0005,
         cliente:{
             id:1,
-            nombre:''
-        }
+            nombre:'Marcos Joule',
+            documento: 1235667
+        },
+        pedido: [ 1, 2, 3
+        ],
+        formaDePago: "Tarjeta Debito",
+        Valor: 560000
     }
 ]
 
 const pedidos
 
-const gastos
+const gastos = [
+    {
+        concepto: "Nomina",
+        valor: 5000000,
+    }
+]
 
-const compras
+const compras = [
+    {
+        insumo: "Pollo",
+        proveedor: "MacPollo",
+        valor: 40000,
+        cantidad: 20,
+        unidad: "kg"
+    }
+]
 
-const clientes
+/*const clientes = [
+    {
 
-const mesas
+    }
+]*/
 
-const categorias
+const mesas = [
+    {
+        numero: 23,
+        ubicacion: "Piso 2",      
+    }
+]
 
-const reporte_de_inventario
+const categorias = [
+        {
+            id: "1",
+            nombre: "Entradas"
+        },
+        {
+            id: "2",
+            nombre: "Plato Fuerte"
+        },
+        {
+            id: "3",
+            nombre: "Postres"
+        }
+]
 
-const usuarios
+const reporte_de_inventario = [
+    {
+        materiasPrimas: {
+            id: "1",
+            nombre: "Carne de res"
+        },
+        productos: {
+            id: "21",
+            nombre: "Agua Mineral"
+        },
+        utileria: {
+            id: "3",
+            nombre: "Cucharas"
+        },
+        mueblesEnseres: {
+            id: "4",
+            nombre: "Mesas de Madera"
+        },
+    }
+]
 
-const rol
+const usuarios= [
+    {
+        id: "1",
+        nombre: "Pedro Paramo",
+        rol: "1"
+    },
+    {
+        id: "2",
+        nombre: "Platon Martinez",
+        rol: "2"
+    },
+    {
+        id: "3",
+        nombre: "Mesero",
+        rol: "3"
+    }
+]
 
-const permisos
+const rol = [
+    {
+        id: "1",
+        nombre: "Administrador"
+    },
+    {
+        id: "2",
+        nombre: "Plato Chef"
+    },
+    {
+        id: "3",
+        nombre: "Mesero"
+    }
+]
 
-const 
+const permisos = [
+    {
+        agregarProductos: {
+            id: "1",
+            nombre: "Atun"
+        },
+        crearVentas: {
+            id: "2",
+            nombre: "Precios"
+        },
+        ModificarPrecios: {
+            id: "3",
+            nombre: "Aumentar IPC"
+        },
+        Facturar: {
+            id: "4",
+            nombre: "Crear Factura"
+        },
+    }
+]
 
 const productos = [
     {
         id:1,
         nombre:'Ensalada',
         precio:12000,
+        categoria: "1"
     },
 
     {
         id:2,
         nombre:'Pollo asado',
-        precio:10000, 
+        precio:10000,
+        categoria: "2"
     },
 
     {
         id:3,
         nombre:'Flan',
         precio:5000,
+        categoria: "3"
     }
 ]
